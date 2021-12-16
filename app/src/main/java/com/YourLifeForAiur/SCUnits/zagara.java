c6c94392640f4c0e555a10579fafa0caea614ee8
YourLifeForAiur <r320stwg@yandex.ru>
2021-12-16 14:52:48 +0500

    иконка, еще активити

---
+++ app/src/main/java/com/YourLifeForAiur/SCUnits/zagara.java
@@ -0,0 +1,14 @@
+package com.YourLifeForAiur.SCUnits;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class zagara extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zagara);
+    }
+}
